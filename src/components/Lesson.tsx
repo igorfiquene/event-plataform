d79f21f83fe74162b1678b5d2f26f746072365cb
igorfiquene <igorfpessoa@gmail.com>
2022-06-23 23:57:49 -0300

    feat: change state if is active lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -12,11 +12,13 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
 	const { slug } = useParams<{ slug: string }>()
-	
+
 	const isLessonAvailable = isPast(props.availableAt);
 	const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
 		locale: ptBr
 	})
+	
+	const isActiveLesson = slug === props.slug
 
 	return (
 		<Link to={`/event/lesson/${props.slug}`} className='group'>
@@ -24,26 +26,26 @@ export function Lesson(props: LessonProps) {
 				{ availableDateFormatted }
 			</span>
 
-			<div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
+			<div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors ${ isActiveLesson ? 'bg-green-500' : '' }`}>
 				<header className="flex items-center justify-between">
 					{ isLessonAvailable ? (
-						<span className="text-sm text-blue-500 font-medium flex align-middle gap-2">
+						<span className={`text-sm font-medium flex align-middle gap-2 ${isActiveLesson ? 'text-white' : 'text-blue-500'}`}>
 							<CheckCircle size={20} />
 							Conteúdo liberado
 						</span>
 					) : (
-						<span className="text-sm text-orange-500 font-medium flex align-middle gap-2">
+						<span className={`text-sm font-medium flex align-middle gap-2 ${isActiveLesson ? 'text-white' : 'text-orange-500'}`}>
 							<Lock size={20} />
 							Em breve
 						</span>
 					)}
 
-					<span className="text-xs rounded px-2 py-[2px] border text-white border-green-300 font-bold">
+					<span className={`text-xs rounded px-2 py-[2px] border text-white font-bold ${ isActiveLesson ? 'border-white' : 'border-green-300' }`}>
 						{ props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA' }
 					</span>
 				</header>
 
-				<strong className="text-gray-200 mt-5 block">
+				<strong className={`mt-5 block ${ isActiveLesson ? 'text-white' : 'text-gray-200' }`}>
 					{ props.title }
 				</strong>
 			</div>
